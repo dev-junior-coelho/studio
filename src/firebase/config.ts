444d98e7f75320e9b71480d8febdef9e8c38575b
Júnior Coelho <gilvanhelsing36@gmail.com>
2025-11-01 16:03:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-878079588-1d0ae",
+  "appId": "1:486175528141:web:4e4d4d291cd8e099c28584",
+  "apiKey": "AIzaSyD_IW8CfZMseq-LsWkQoZnzEobByPywbss",
+  "authDomain": "studio-878079588-1d0ae.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "486175528141"
+};
